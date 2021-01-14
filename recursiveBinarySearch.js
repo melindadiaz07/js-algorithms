f7276c87623a9f8f24365665399eb960c7d5e2ab
Melindadiaz07 <melindadiaz@me.com>
2021-01-14 15:46:15 -0500

    initial commit

---
+++ recursiveBinarySearch.js
@@ -0,0 +1,18 @@
+
+function recursiveSearch (array, element, firstIndex=0, lastIndex=array.length-1){
+
+if(firstIndex > lastIndex) { return "Not in array" }
+  
+const middle = Math.floor((firstIndex + lastIndex)/2)
+
+if (element === array[middle]) { return middle }
+
+ if (element > array[middle]){
+   return recursiveSearch(array, element, middle+1, lastIndex) 
+ } else {
+   return recursiveSearch(array, element, firstIndex, middle-1)
+ } 
+}
+
+const array = [1, 2, 3, 4, 5, 6, 7]
+console.log(recursiveSearch(array, 9))
