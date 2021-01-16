6620560476dbcb86e71ea90d79459360e91e6398
Melindadiaz07 <melindadiaz@me.com>
2021-01-16 14:53:32 -0500

    Added working recursive palindrome function

---
+++ palindrome.js
@@ -0,0 +1,15 @@
+function checkPalindrome(str){
+  if (str <= 1) return true
+
+//   console.log(str.slice(1, str.length-1))
+  return(str[0] === str[str.length-1] ? 
+  checkPalindrome(str.slice(1, str.length-1)) :
+  false
+   )
+}
+
+(checkPalindrome(tacocat))
+
+
+// tacocat tt aa cc o
+// toot tt oo 
