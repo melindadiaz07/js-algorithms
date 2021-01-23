6b3fa76431435e736a3f747820dceb6cee30d863
Melindadiaz07 <melindadiaz@me.com>
2021-01-23 16:33:26 -0500

    Wrote out notes for efficient solution

--- countDuplicates.js
+++ countDuplicates.js
@@ -2,3 +2,7 @@
 // and numeric digits that occur more than once in the input string. The input string can be assumed 
 // to contain only alphabets (both uppercase and lowercase) and numeric digits.
 
+function countDuplicates(string){
+  // cereate hash table  where key is letter or number, value is count
+  // return only those with values higher than 1
+}
